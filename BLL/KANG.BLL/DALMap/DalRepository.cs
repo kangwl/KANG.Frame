3c988f97ce3e22b2e985b88d9b98ef1fc36aefcc
kangwl <kangwl2009@163.com>
2015-10-22 17:17:47 +0800

    kangwl

--- BLL/KANG.BLL/DALMap/DalRepository.cs
+++ BLL/KANG.BLL/DALMap/DalRepository.cs
@@ -1,11 +1,13 @@
-﻿using KANG.IDAL;
+﻿using System;
+using KANG.IDAL;
 
 namespace KANG.BLL.DALMap {
+
     /// <summary>
     /// 生成DAL的操作
     /// 可更换数据库
     /// </summary>
-    public class DalRepository {
+    public partial class DalRepository {
 
 
         private static IUser<MODEL.User_MODEL> userOperate;
@@ -16,6 +18,8 @@ namespace KANG.BLL.DALMap {
             //get { return userOperate ?? (userOperate = CreateInstance<KANG.MySqlDAL.User_DAL>()); }
             get { return userOperate ?? (userOperate = CreateInstance<KANG.EFDAL.User_DAL>()); }
         }
+
+
 
 
         /// <summary>
